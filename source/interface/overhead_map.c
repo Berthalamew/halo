dbfd7924ab92fd17cff031b833cc35823f96eaa9
punpckhdq <121350336+punpckhdq@users.noreply.github.com>
2026-07-30 17:04:13 +0100

    Some overhead_map.c
    Doesn't exist in any other version idk

--- source/interface/overhead_map.c
+++ source/interface/overhead_map.c
@@ -18,6 +18,9 @@ symbols in this file:
 
 /* ---------- headers */
 
+#include "cseries.h"
+#include "game.h"
+
 /* ---------- constants */
 
 /* ---------- macros */
@@ -28,6 +31,61 @@ symbols in this file:
 
 /* ---------- globals */
 
+struct
+{
+	boolean bss_00453cd0;
+	short data_853cd2;
+	short data_853cd4;
+	short data_853cd6;
+	long data_853cd8;
+	long data_853cdc;
+	long data_853ce0;
+	double data_853ce4;
+	short data_853cec;
+	short data_853cee;
+	long data_853cf0;
+	void* data_853cf8;
+} overhead_map_globals;
+
 /* ---------- public code */
+
+void overhead_map_initialize(
+	void)
+{
+	return;
+}
+
+void overhead_map_initialize_for_new_map(
+	void)
+{
+	return;
+}
+
+void overhead_map_dispose_from_old_map(
+	void)
+{
+	overhead_map_globals.bss_00453cd0 = FALSE;
+
+	return;
+}
+
+void overhead_map_post_rasterize(
+	void* a1,
+	void* a2,
+	double* a3)
+{
+	double* v1;
+	void* v2;
+
+	if (overhead_map_globals.bss_00453cd0)
+	{
+		overhead_map_globals.data_853ce4 = *a3;
+
+		v1 = a3;
+		v2 = overhead_map_globals.data_853cf8;
+	}
+
+	return;
+}
 
 /* ---------- private code */
