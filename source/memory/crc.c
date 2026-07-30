782a7638f9341a3ecb60812b58837a3ff66e9f6e
punpckhdq <121350336+punpckhdq@users.noreply.github.com>
2026-07-30 23:50:06 +0100

    crc.c

--- source/memory/crc.c
+++ source/memory/crc.c
@@ -1,33 +1,95 @@
 /*
 CRC.C
-
-symbols in this file:
-001088D0 0010:
-	_crc_new (0000)
-001088E0 0040:
-	_code_001088e0 (0000)
-00108920 0080:
-	_crc_checksum_buffer (0000)
-0027D2E4 000f:
-	??_C@_0P@JPGOHOCM@buffer_size?$DO?$DN0?$AA@ (0000)
-0027D2F4 001c:
-	??_C@_0BM@FPJPBIIF@c?3?2halo?2SOURCE?2memory?2crc?4c?$AA@ (0000)
-00456220 0401:
-	_bss_00456220 (0000)
 */
 
 /* ---------- headers */
 
+#include "cseries.h"
+#include "crc.h"
+
 /* ---------- constants */
 
-/* ---------- macros */
-
-/* ---------- structures */
+enum
+{
+	CRC_NEW = -1,
+	CRC_TABLE_SIZE = 256,
+	CRC32_POLYNOMIAL = 0xEDB88320
+};
 
 /* ---------- prototypes */
 
-/* ---------- globals */
+static void build_crc_table(unsigned long *crc_table);
 
 /* ---------- public code */
 
+void crc_new(
+	unsigned long *crc_reference)
+{
+	*crc_reference = CRC_NEW;
+
+	return;
+}
+
+void crc_checksum_buffer(
+	unsigned long *crc_reference,
+	void const *buffer,
+	long buffer_size)
+{
+	static unsigned long crc_table[256];
+	static boolean crc_table_built;
+
+	unsigned long crc;
+
+	match_assert("c:\\halo\\SOURCE\\memory\\crc.c", 42, buffer_size>=0);
+
+	if (!crc_table_built)
+	{
+		build_crc_table(crc_table);
+		crc_table_built = TRUE;
+	}
+
+	crc = *crc_reference;
+
+	if (buffer_size>0)
+	{
+		byte *data = (byte*)buffer;
+		
+		while (buffer_size--) 
+		{
+			unsigned long v7 = crc_table[(crc ^ *data++) & 0xFF];
+			crc = (crc>>8) ^ v7;
+		}
+	}
+
+	*crc_reference = crc;
+
+	return;
+}
+
 /* ---------- private code */
+
+static void build_crc_table(
+	unsigned long *crc_table)
+{
+	short table_index;
+
+	for (table_index = 0; table_index<CRC_TABLE_SIZE; ++table_index)
+	{
+		unsigned long crc = table_index;
+		short round;
+
+		for (round = 0; round<8; ++round)
+		{
+			if (crc&1)
+			{
+				crc = (crc>>1) ^ CRC32_POLYNOMIAL;
+			}
+			else
+			{
+				crc = crc>>1;
+			}
+		}
+
+		crc_table[table_index] = crc;
+	}
+}
--- source/memory/crc.h
+++ source/memory/crc.h
@@ -1,26 +1,14 @@
 /*
 CRC.H
-
-header included in hcex build.
 */
 
 #ifndef __CRC_H
 #define __CRC_H
 #pragma once
 
-/* ---------- constants */
-
-/* ---------- macros */
-
-/* ---------- structures */
-
 /* ---------- prototypes/CRC.C */
 
 void crc_new(unsigned long *crc_reference);
 void crc_checksum_buffer(unsigned long *crc_reference, void const *buffer, long buffer_size);
-
-/* ---------- globals */
-
-/* ---------- public code */
 
 #endif // __CRC_H
